54d1cef7cc259bccaa3aae340cbf5a0a9ceaa6e0
Gabriel Humeniuc <gabi@work.ro>
2021-11-01 09:13:05 +0200

    First commit

---
+++ cdinout.sh
@@ -0,0 +1,53 @@
+cdinout_prompt_command() {
+    if [ "$CDINOUT_PATH" == "$PWD" ]; then
+        echo "same dir"
+        return
+    fi   
+
+    cmdinout_execute_out
+    cmdinout_execute_in 
+}
+
+cmdinout_execute_out() {
+    if [ -v "CDINOUT_PATH" ]; then
+        # Remove first slash
+        DIR="${CDINOUT_PATH/\//}"
+
+        # Build path to the out script
+        CDINOUT_OUT_PATH="$HOME/.cdinout/scripts/$DIR/out.sh"
+        echo "CDINOUT_OUT_PATH=$CDINOUT_OUT_PATH"
+
+        # Run the out script if exists
+        if [ -f "$CDINOUT_OUT_PATH" ]; then
+            source "$CDINOUT_OUT_PATH"
+        fi
+
+        unset "CDINOUT_PATH"
+    fi   
+
+    return
+}
+
+cmdinout_execute_in() {
+    CURDIR="$PWD"
+    until [ "$CURDIR" = "" ]; do
+        # Remove first slash
+        DIR="${CURDIR/\//}"
+
+        # Build path to the in script
+        CDINOUT_IN_PATH="$HOME/.cdinout/scripts/$DIR/in.sh"
+
+        # Run the in script, if exists.
+        # Set the env var.
+        if [ -f "$CDINOUT_IN_PATH" ]; then
+            source $CDINOUT_IN_PATH
+            export CDINOUT_PATH="$CURDIR"
+            break;
+        fi
+
+        CURDIR="${CURDIR%/*}"
+    done
+}
+
+cdinout_prompt_command_cmd=$'\n''cdinout_prompt_command'$'\n'
+PROMPT_COMMAND="${PROMPT_COMMAND/$cdinout_prompt_command_cmd/}$cdinout_prompt_command_cmd"
