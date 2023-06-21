cfc55ff51f9076c8b0b8909173b45d9ce01490e1
Gabriel Humeniuc <gabi@work.ro>
2023-06-21 09:35:02 +0300

    refactoring

--- cdinout.sh
+++ cdinout.sh
@@ -1,38 +1,33 @@
 # shellcheck shell=bash
 
 __cdinout_prompt_command() {
-    local CURDIR="$PWD"
     local CDINOUT_SCRIPTS_PATH="$HOME/.config/cdinout/paths"
 
-    __cdinout_debug() {
-        [ ! -z "$CDINOUT_DEBUG" ] && echo "CDINOUT: $1" >&2
+    __cdinout_check_same_dir()
+    {
+        __cdinout_debug "scanning"
+        local CURDIR="$PWD"
+
+        # Test if I am in a $CDINOUT_PATH subdirectory
+        if [ -v "CDINOUT_PATH" ]; then
+            until [ "$CURDIR" = "" ]; do
+                __cdinout_debug "Testing $CURDIR"
+                if [ "$CDINOUT_PATH" == "$CURDIR" ]; then
+                    __cdinout_debug "same dir. exit"
+                    return 0
+                fi
+
+                CURDIR="${CURDIR%/*}"
+            done
+        fi
+
+        return 1
     }
 
-    __cdinout_debug "scanning"
-
-    # Test if I am in a $CDINOUT_PATH subdirectory
-    if [ -v "CDINOUT_PATH" ]; then
-        until [ "$CURDIR" = "" ]; do
-            __cdinout_debug "Testing $CURDIR"
-            if [ "$CDINOUT_PATH" == "$CURDIR" ]; then
-                __cdinout_debug "same dir. exit"
-                return
-            fi
-
-            CURDIR="${CURDIR%/*}"
-        done
-    fi
-
-    __cdinout_dir_path() {
-        # Remove first slash
-        DIR="${1/\//}"
-
-        # Build path to the out script
-        echo "$CDINOUT_SCRIPTS_PATH/$DIR"
-    }
 
     # Out method
-    __cdinout_out() {
+    __cdinout_out()
+    {
         # Not in a special dir. Skip
         if [ ! -v "CDINOUT_PATH" ]; then
             return
@@ -52,13 +47,15 @@ __cdinout_prompt_command() {
     }
 
     # In method
-    __cdinout_in() {
+    __cdinout_in()
+    {
         # Allready in a special dir. Skip
         if [ -v "CDINOUT_PATH" ]; then
             return
         fi
 
-        CURDIR="$PWD"
+        local CURDIR="$PWD"
+
         until [ "$CURDIR" = "" ]; do
             local CDINOUT_IN_PATH
             CDINOUT_IN_PATH="$(__cdinout_dir_path "$CURDIR")/in.sh"
@@ -76,13 +73,58 @@ __cdinout_prompt_command() {
         done
     }
 
-    __cdinout_out
-    __cdinout_in
 
+    __cdinout_dir_path()
+    {
+        # Remove first slash
+        DIR="${1/\//}"
+
+        # Build path to the out script
+        echo "$CDINOUT_SCRIPTS_PATH/$DIR"
+    }
+
+
+    # Prepend PATH with a path
+    __cdinout_path_prepend()
+    {
+        __cdinout_debug "prepend $1"
+
+        __cdinout_path_remove "$1"
+        export PATH="$1:$PATH"
+        unset LOCAL_PATH
+    }
+
+
+    # Remove a path from PATH
+    __cdinout_path_remove()
+    {
+        __cdinout_debug "remove $1"
+
+        PATH=":$PATH:"
+        PATH="${PATH/:$1:/:}"
+        PATH="${PATH#:}"
+        PATH="${PATH%:}"
+        export PATH="$PATH"
+    }
+
+
+    __cdinout_debug() {
+        [ ! -z "$CDINOUT_DEBUG" ] && echo "CDINOUT: $1" >&2
+    }
+
+
+    if ! __cdinout_check_same_dir; then
+        __cdinout_out
+        __cdinout_in
+    fi
+
+    unset -f __cdinout_path_prepend
+    unset -f __cdinout_path_remove
     unset -f __cdinout_dir_path
+    unset -f __cdinout_debug
     unset -f __cdinout_out
     unset -f __cdinout_in
-    unset -f __cdinout_debug
+    unset -f __cdinout_check_same_dir
 }
 
 # Append command to PROMPT_COMMAND.
